cda01b1cbc4c0e75d5ecd5da7c64b7b2258b1cb0
hemanth-2113 <hemanth02135@gmail.com>
2025-08-06 18:49:36 +0530

    vectors_hasmaps

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+// use std::collections::HashMap;
+
+
+
+
+// fn main() {
+
+// let mut useres=HashMap::new();
+// // useres.insert(String::from("hemanth"), 20);
+// useres.insert(String::from("jerusha"), 20);
+
+// let first_user_age=useres.get("hemanth");
+
+//   match first_user_age{
+//     Some(age)=>println!("age is {}",age),
+//     None=>println!("invaid key")
+//   }
+
+// }
+
+// use std::collections::HashMap;
+
+
+
+// fn group_value_keys(vec:Vec<(String,i32)>)->HashMap<String,i32>{
+//     let mut hm=HashMap::new();
+//     for (key ,value) in vec{
+//         hm.insert( key,value);
+//     }
+//     return hm;
+// }
+
+
+// fn main(){
+
+
+// let input_vec=vec![(String::from("hemnath"),22),(String::from("jerusha"),20)];
+// let hm=group_value_keys(input_vec);
+// println!("{:?}",hm)
+// }
+
+fn main(){
+    println!("hello world");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
